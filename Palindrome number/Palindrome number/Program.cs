69b50adb430e6445ac39b7825e6a920f3118017d
Anshu446 <115145258+Anshu446@users.noreply.github.com>
2022-12-16 06:53:05 +0000

    palindrome number

---
+++ Palindrome number/Palindrome number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_number
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int a = 121; int b; int result = 0; int temp = a;
+
+                while (a != 0) { b = a % 10; result = result * 10 + b; a = a / 10; }
+
+                a = temp;
+
+               
+            if (a == result)
+              {
+                Console.WriteLine("Palindrome Number");
+              } 
+            else 
+            {
+                Console.WriteLine("Not a palindrome Numbber");
+            }
+                Console.ReadLine();
+            }
+        }
+    }
+
+
